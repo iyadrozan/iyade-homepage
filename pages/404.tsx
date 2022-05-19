a16a074371e978511edefd4a79492161058f9623
iyadrozan <iyadrozan26@gmail.com>
2022-05-20 00:11:09 +0800

    add fontawesome

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,25 @@
 
 import styles from '../styles/404.module.css'
 import Image from 'next/image'
-
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 export default function Custom404() {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 5000);
+    }, []);
+
     return (
         <div className='container'>
-            <section className={styles.center}>
+            <section className={styles.custom404}>
                 <Image src='/image/404.svg' width={500} height={500} alt="logo" />
-                <h1 className='indonesia'>error page...</h1>
+                <a className='btn btn-default' href="/"><FontAwesomeIcon icon={faHouse} /> Back to home</a>
             </section>
         </div>
     )
